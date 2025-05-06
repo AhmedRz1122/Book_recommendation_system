7b82fee662d35e1fd5b5f33c6e24e4a8fd0a8a02
AhmedRz1122 <ar9192051@gmail.com>
2025-05-06 21:05:47 +0500

    Code Updated

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,13 +5,13 @@ import './Home.css'
 const Home = () => {
   return (
     <div className='min-h-screen w-screen bg-gradient-to-r from-blue-600 to-purple-600 text-white m-0'>
-      <nav className='bg-amber-200 flex w-full h-14 text-black items-center'>
-        <div className='flex absolute left-20 gap-x-4 items-center'>
+      <nav className='bg-amber-200 flex w-full h-14 text-black items-center px-2.5 justify-between'>
+        <div className='flex gap-x-4 items-center '>
           <img src={booklogo} alt="Main Logo of Book Recommendor" className='h-12 w-12 rounded-full' />
           <h1>Intelligent Book Recommendation System</h1>
         </div>
-        <div>
-          <ul className='flex absolute right-10 gap-x-4'>
+        <div className='flex items-center'>
+          <ul className='flex gap-x-4'>
             <li>Home</li>
             <li>About</li>
             <li>Login</li>
@@ -24,11 +24,11 @@ const Home = () => {
         <div className="p-6 text-white">
           <h1 className='text-2xl font-bold mb-4'>Discover Your Next Favourite Book</h1>
           <p className='mb-4'>Tell us about the type of book you're looking for and the emotions you want to feel</p>
-          <div className="flex flex-col items-start">  {/* Updated container */}
+          <div className="flex flex-col items-center">  {/* Updated container */}
             <input
               type="text"
               placeholder='Book Description'
-              className="p-2 rounded text-black border-2 border-solid border-amber-50 w-full mb-3"
+              className="p-2 rounded text-black border-2 border-solid border-amber-50 w-full mb-3 placeholder:text-white"
             />
             <button className='bg-amber-200 text-black p-2 rounded hover:bg-amber-300'>
               Get Recommendations
